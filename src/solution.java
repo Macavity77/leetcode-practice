743edccd3b192632e532735c3b46817f85c4f4a9
zeflg <zw6aw@virginia.edu>
2018-08-13 00:45:01 -0400

    za6 modified

--- src/solution.java
+++ src/solution.java
@@ -432,49 +432,37 @@ public class solution {
     }
 
     public static String minWindow(String s, String t) {
-
-        int s_len = s.length();
-        int t_len = t.length();
-        int ruler = t_len;
-
-        int res = Integer.MAX_VALUE;
+        int slen = s.length();
+        int tlen = t.length();
+        int matcher = tlen;
         int start = 0;
-
-        int[] freq = new int[128];
-
-        char[] c_s = s.toCharArray();
-        char[] c_t = t.toCharArray();
-
-        // count char in t
-        for (char c : c_t)
-            freq[c]++;
-
-        int i = 0, j = 0;
-        while (j < s_len) {
-
-            // whether ruler can expand
-            if (freq[c_s[j++]]-- >= 1)
-                ruler--;
-
-            // if ruler == 0, it means all char in t has been contained in window now
-            while (ruler == 0) {
-
-                // choose min window
-                if (res > j - i) {
-                    res = j - i;
-                    start = i;
-                }
-
-                // narrow left border
-                if (freq[c_s[i++]]++ == 0)
-                    ruler++;
-
-            }
-
+        int result = Integer.MAX_VALUE;
+        char[] sc = s.toCharArray();
+        char[] tc = t.toCharArray();
+        int[] map = new int[128];
+        for (char c : tc) {
+            map[c]++;
         }
 
-        return res == Integer.MAX_VALUE ? "" : s.substring(start, start + res);
+        int i = 0, j = 0;
+        while (j < slen) {
+            if (map[sc[j++]]-- >= 1) {
+                matcher--;
+            }
+            //all solved
+            while (matcher == 0) {
+                if (result > j - i) {
+                    result = j - i;
+                    start = i;
+                }
+                //try shrinking,外面有个while就可以起到不断shrink的作用
+                if (map[sc[i++]]++ == 0) {
+                    matcher++;
+                }
+            }
+        }
 
+        return result == Integer.MAX_VALUE? "" : s.substring(start, start + result);
     }
 
     public static void main(String[] args) {
